f7030a12d64d75747ef7d31a6e92d880aaeb51e0
Alina Siamenava <Amentyuk.19@gmail.com>
2021-11-03 11:45:23 +0300

    3 task 4 lesson bags fixed

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,5 +1,5 @@
 function highlight(table) {
-	let status = document.querySelectorAll('tbody tr > td:last-child');
+	let status = table.querySelectorAll('tbody tr > td:last-child');
 	for(let i = 0; i < status.length; i++){
 		let statusData = status[i].getAttribute('data-available');
 		if(statusData == 'true'){
@@ -12,7 +12,7 @@ function highlight(table) {
 			status[i].parentNode.setAttribute('hidden', 'hidden'); 
 		}
 
-		let gender = document.querySelectorAll('tbody tr > td:nth-child(3)');
+		let gender = table.querySelectorAll('tbody tr > td:nth-child(3)');
 		for(let i = 0; i < gender.length; i++){
 			if(gender[i].innerText == 'm'){
 				gender[i].parentNode.classList.add('male');
@@ -21,7 +21,7 @@ function highlight(table) {
 				gender[i].parentNode.classList.add('female');
 			}
 		}
-		let age = document.querySelectorAll('tbody tr > td:nth-child(2)');
+		let age = table.querySelectorAll('tbody tr > td:nth-child(2)');
 		for(let i = 0; i < age.length; i++){
 			let ageNumber = Number(age[i].innerText);
 			if(ageNumber < 18){
